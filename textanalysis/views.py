c1a8155895f183ac439ec4e61b7fab3c91f1cdc9
Giovanni Toffoli <toffoli@linkroma.it>
2023-07-22 01:07:12 +0200

    Fixed url of api function Text Cohesion.

--- textanalysis/views.py
+++ textanalysis/views.py
@@ -50,11 +50,11 @@ obj_type_label_dict = {
     '': '?',
 }
 
-# from: https://sashamaps.net/docs/resources/20-colors/
+# from: https://sashamaps.net/docs/resources/20-colors/ (made minor changes)
 distinct_colors = [
   ['Red', '#e6194b'], ['Green', '#3cb44b'], ['Yellow', '#ffe119'], ['Blue', '#4363d8'], ['Orange', '#f58231'],
   ['Purple', '#911eb4'], ['Cyan', '#46f0f0'], ['Magenta', '#f032e6'], ['Lime', '#bcf60c'], ['Pink', '#fabebe'],
-  ['Teal', '#008080'], ['Lavender', '#e6beff'], ['Brown', '#9a6324'], ['Beige', '#fffac8'], ['Maroon', '#800000'],
+  ['Teal', '#008080'], ['Lavender', '#e6beff'], ['Brown', '#9a6324'], ['Dark Beige', '#9f8c76'], ['Maroon', '#800000'],
   ['Mint', '#aaffc3'], ['Olive', '#808000'], ['Apricot', '#ffd8b1'], ['Navy', '#000075'], ['Grey', '#808080'],
   ['White', '#ffffff'], ['Black', '#000000'],
 ]
